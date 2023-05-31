830d959e789223e45c194d64b31861c7cb638fac
Jack <leoncoolmoon@hotmail.com>
2023-05-31 18:54:41 +1000

    Update key.h
    add interruption for the key, and save the language setting. now you can press key0 when reading to pause the reading or press it when finish the story to change the language. an internal LED will lid up for English and turn off for Chinese on start or on change. the change of language will apply to the next play. you can change the story by reset

--- GPTStoryTeller/key.h
+++ GPTStoryTeller/key.h
@@ -1,13 +1,29 @@
 #include <Arduino.h>
-
-const int storyKey = 2;
-void setupKey() {
- pinMode(storyKey, INPUT);
-}
-bool loopKey() {
-  if (digitalRead(storyKey) != HIGH ) {
-    Serial.println(" --- storyKey pressed");
-    return true;
+#include <EEPROM.h>            // read and write from flash memory
+#define EEPROM_SIZE 1
+#define  storyKey 0
+void changeLanguage() {
+  CN = !CN;
+  pinMode(15, OUTPUT);
+  digitalWrite(15, CN?LOW:HIGH);
+  EEPROM.write(0, CN);
+  EEPROM.commit();
+};
+void IRAM_ATTR play_Pause_Story()
+{ if (!finish) {
+    play = !play;
+  } else {
+    changeLanguage();
   }
-  return false;
+  Serial.println(" --- storyKey pressed");
 }
+void setupKey() {
+  EEPROM.begin(EEPROM_SIZE);
+  CN = EEPROM.read(0);
+  pinMode(storyKey, INPUT_PULLUP);
+  attachInterrupt(storyKey, play_Pause_Story, RISING);
+  pinMode(15, OUTPUT);
+  digitalWrite(15, CN?LOW:HIGH);
+}
+
+
